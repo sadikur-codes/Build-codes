5dc8d73458636eaef0964de2754499d1b6968d20
sadikur-codes <sadikur.codes@gmail.com>
2025-12-24 11:09:07 +0600

    footer and hero added on html and css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,29 @@
-$(function(){
-    
-});
+/* hero slider start */
 
+// image sources array
+const heroImages = [
+    "http://127.0.0.1:5500/assets/images/hero/loq.png",
+    "http://127.0.0.1:5500/assets/images/hero/tuf.png"
+];
 
+let currentIndex = 0;
+
+// get image element
+const heroImage = document.getElementById("heroImage");
+
+// set initial image
+heroImage.src = heroImages[currentIndex];
+
+// change image every 3 seconds
+setInterval(() => {
+    currentIndex++;
+
+    // loop back to first image
+    if (currentIndex >= heroImages.length) {
+        currentIndex = 0;
+    }
+
+    heroImage.src = heroImages[currentIndex];
+}, 3000);
+
+/* hero slider end */
